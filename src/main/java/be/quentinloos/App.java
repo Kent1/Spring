d02e26654ec9e2c7b8ccfb5a6056c691d3f894dc
Quentin Loos <quentin.loos@dnsbelgium.be>
2017-10-07 15:39:06 +0200

    Remove ApplicationContext.xml and add JavaConfig class

--- src/main/java/be/quentinloos/App.java
+++ src/main/java/be/quentinloos/App.java
@@ -2,7 +2,7 @@ package be.quentinloos;
 
 import be.quentinloos.service.FleetService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Hello world!
@@ -12,7 +12,7 @@ public class App {
 //    DriverRepository driverRepository = new DriverRepositoryImpl();
 //    FleetService fleetService = new FleetServiceImpl(driverRepository);
 
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
     FleetService fleetService = applicationContext.getBean(FleetService.class);
     fleetService.getAllDrivers().forEach(System.out::println);
   }
---
+++ src/main/java/be/quentinloos/AppConfig.java
@@ -0,0 +1,9 @@
+package be.quentinloos;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("be.quentinloos")
+public class AppConfig {
+}
