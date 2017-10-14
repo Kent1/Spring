5d1cdb12ce9347f44cad6ff77901e591b4dde7c4
Quentin Loos <quentin.loos@dnsbelgium.be>
2017-10-14 18:50:36 +0200

    Remove applicationContext.xml and use JavaConfig

--- src/main/java/be/quentinloos/AppConfig.java
+++ src/main/java/be/quentinloos/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,6 +15,7 @@ import java.util.List;
 @Configuration
 @ComponentScan("be.quentinloos")
 @PropertySource("classpath:/application.properties")
+@EnableWebMvc
 public class AppConfig {
 
   @Bean
