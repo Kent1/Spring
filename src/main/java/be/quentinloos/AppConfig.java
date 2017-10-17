533e1624a3baac658b769ce0b2c79768b20f264f
Quentin Loos <quentin.loos@dnsbelgium.be>
2017-10-17 13:07:37 +0200

    Useless annotations

--- src/main/java/be/quentinloos/AppConfig.java
+++ src/main/java/be/quentinloos/AppConfig.java
@@ -3,15 +3,7 @@ package be.quentinloos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration
-@ComponentScan("be.quentinloos")
-@PropertySource("classpath:/application.properties")
-@EnableWebMvc
 @SpringBootApplication
 public class AppConfig extends SpringBootServletInitializer {
 
