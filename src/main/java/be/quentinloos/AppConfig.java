f37f0ce9bc4d0830a8ed89399182d3b3ff92e9e0
Quentin Loos <quentin.loos@dnsbelgium.be>
2017-10-17 13:04:28 +0200

    Use @SpringBootApplication

--- src/main/java/be/quentinloos/AppConfig.java
+++ src/main/java/be/quentinloos/AppConfig.java
@@ -1,31 +1,22 @@
 package be.quentinloos;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 @Configuration
 @ComponentScan("be.quentinloos")
 @PropertySource("classpath:/application.properties")
 @EnableWebMvc
-public class AppConfig implements WebApplicationInitializer {
+@SpringBootApplication
+public class AppConfig extends SpringBootServletInitializer {
 
-  @Override
-  public void onStartup(ServletContext servletContext) throws ServletException {
-    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-    context.register(AppConfig.class);
-    servletContext.addListener(new ContextLoaderListener(context));
-
-    ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
-    dispatcher.addMapping("/");
+  public void main(String[] args) {
+    SpringApplication.run(AppConfig.class, args);
   }
+
 }
